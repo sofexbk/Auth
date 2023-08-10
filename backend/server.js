b4c32517b90d9bff353920c3a24cbe5135c5b98f
soufianbouktaib1@gmail.com <109437519+sofexbk@users.noreply.github.com>
2023-08-10 16:22:54 +0100

    user model

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express=require('express')
 const mongoose=require('mongoose')
 const WorkoutRoutes=require('./routes/workouts')
-
+const userRoutes=require('./routes/user')
 //express app
 const app=express()
 
@@ -17,6 +17,7 @@ app.use((req,res,next)=>{
 
 //routes
 app.use('/api/workouts',WorkoutRoutes)
+app.use('/api/user',userRoutes)
 
 
 //connect to db
