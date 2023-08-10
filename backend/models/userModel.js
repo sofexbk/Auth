6fd12063a9d352b856db989ac68bd07d93fb8ee3
soufianbouktaib1@gmail.com <109437519+sofexbk@users.noreply.github.com>
2023-08-10 22:43:46 +0100

    jwt + authcontext

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -16,7 +16,6 @@ const userSchema=new Schema({
 })
 
 //static signup method
-
 userSchema.statics.signup=async function(email,password){
 
   //validation
@@ -44,4 +43,25 @@ userSchema.statics.signup=async function(email,password){
   return user
 } 
 
+
+//static login method
+userSchema.statics.login=async function(email,password){
+    if(!email||!password){
+        throw Error('All fields must be filled')
+    }
+
+    const user=await this.findOne({ email})
+    if(!user){
+      throw Error('Incorrect Email')
+    } 
+
+    const match=await bcrypt.compare(password,user.password)
+     if(!match){
+        throw Error('Incorrect password')
+    }
+
+
+    return user
+
+}
 module.exports=mongoose.model('User',userSchema)
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { WorkoutProvider } from './context/WorkoutContext';
+import { AuthProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <WorkoutProvider>
+   <AuthProvider>
+     <WorkoutProvider>
         <App />
-    </WorkoutProvider>
-
+     </WorkoutProvider>
+   </AuthProvider>
   </React.StrictMode>
 );
