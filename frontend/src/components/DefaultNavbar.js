a7bee52b3c9f1dd71a19ef397a95912263305a53
soufianbouktaib1@gmail.com <109437519+sofexbk@users.noreply.github.com>
2023-08-11 16:41:44 +0100

    useLogoutHook done

--- frontend/src/components/DefaultNavbar.js
+++ frontend/src/components/DefaultNavbar.js
@@ -1,8 +1,15 @@
 import {Link} from 'react-router-dom'
 import Login from '../pages/Login'
 import Signup from '../pages/Signup'
+import { useLogout } from '../hooks/useLogout'
+
 
 const DefaultNavbar = () => {
+   const {logout}=useLogout()
+
+  const handleClick=()=>{
+    logout()
+  }
   return (
 
      <nav>
@@ -13,6 +20,9 @@ const DefaultNavbar = () => {
            <div className="hidden md:flex justify-around space-x-4">
              <Link to ="/" className="hover:text-indigo-600 text-gray-700">Home</Link>
            </div>
+         </div>
+         <div>
+            <button onClick={handleClick}>Log Out</button>
          </div>
          <div className="flex space-x-4 items-center">
            <Link to="/Login" className="text-gray-800 text-sm">LOGIN</Link>
---
+++ frontend/src/hooks/useLogout.js
@@ -0,0 +1,23 @@
+import { useAuthContext } from "./useAuthContext"
+
+
+export  const useLogout=()=>{
+
+    const {dispatch}=useAuthContext()
+
+    const logout=()=>{
+      //remove user from storage 
+      localStorage.removeItem('user')
+
+      //dispatch logout action
+      dispatch({type:'LOGOUT'})
+    }
+    return {logout}
+}
+
+
+
+
+
+//update globale state 
+//delete token
